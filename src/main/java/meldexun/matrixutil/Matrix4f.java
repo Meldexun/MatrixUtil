0092e634b260b1b2c55bcdff7a4fe8c2876a7d5c
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-12-09 01:17:29 +0100

    Reduce field access when multiplying matrices

--- src/main/java/meldexun/matrixutil/Matrix4f.java
+++ src/main/java/meldexun/matrixutil/Matrix4f.java
@@ -313,38 +313,54 @@ public class Matrix4f {
 	}
 
 	public void multiply(Matrix4f matrix) {
-		float f00 = this.m00 * matrix.m00 + this.m01 * matrix.m10 + this.m02 * matrix.m20 + this.m03 * matrix.m30;
-		float f01 = this.m00 * matrix.m01 + this.m01 * matrix.m11 + this.m02 * matrix.m21 + this.m03 * matrix.m31;
-		float f02 = this.m00 * matrix.m02 + this.m01 * matrix.m12 + this.m02 * matrix.m22 + this.m03 * matrix.m32;
-		float f03 = this.m00 * matrix.m03 + this.m01 * matrix.m13 + this.m02 * matrix.m23 + this.m03 * matrix.m33;
-		float f10 = this.m10 * matrix.m00 + this.m11 * matrix.m10 + this.m12 * matrix.m20 + this.m13 * matrix.m30;
-		float f11 = this.m10 * matrix.m01 + this.m11 * matrix.m11 + this.m12 * matrix.m21 + this.m13 * matrix.m31;
-		float f12 = this.m10 * matrix.m02 + this.m11 * matrix.m12 + this.m12 * matrix.m22 + this.m13 * matrix.m32;
-		float f13 = this.m10 * matrix.m03 + this.m11 * matrix.m13 + this.m12 * matrix.m23 + this.m13 * matrix.m33;
-		float f20 = this.m20 * matrix.m00 + this.m21 * matrix.m10 + this.m22 * matrix.m20 + this.m23 * matrix.m30;
-		float f21 = this.m20 * matrix.m01 + this.m21 * matrix.m11 + this.m22 * matrix.m21 + this.m23 * matrix.m31;
-		float f22 = this.m20 * matrix.m02 + this.m21 * matrix.m12 + this.m22 * matrix.m22 + this.m23 * matrix.m32;
-		float f23 = this.m20 * matrix.m03 + this.m21 * matrix.m13 + this.m22 * matrix.m23 + this.m23 * matrix.m33;
-		float f30 = this.m30 * matrix.m00 + this.m31 * matrix.m10 + this.m32 * matrix.m20 + this.m33 * matrix.m30;
-		float f31 = this.m30 * matrix.m01 + this.m31 * matrix.m11 + this.m32 * matrix.m21 + this.m33 * matrix.m31;
-		float f32 = this.m30 * matrix.m02 + this.m31 * matrix.m12 + this.m32 * matrix.m22 + this.m33 * matrix.m32;
-		float f33 = this.m30 * matrix.m03 + this.m31 * matrix.m13 + this.m32 * matrix.m23 + this.m33 * matrix.m33;
-		this.m00 = f00;
-		this.m01 = f01;
-		this.m02 = f02;
-		this.m03 = f03;
-		this.m10 = f10;
-		this.m11 = f11;
-		this.m12 = f12;
-		this.m13 = f13;
-		this.m20 = f20;
-		this.m21 = f21;
-		this.m22 = f22;
-		this.m23 = f23;
-		this.m30 = f30;
-		this.m31 = f31;
-		this.m32 = f32;
-		this.m33 = f33;
+		float a00 = this.m00;
+		float a01 = this.m01;
+		float a02 = this.m02;
+		float a03 = this.m03;
+		float a10 = this.m10;
+		float a11 = this.m11;
+		float a12 = this.m12;
+		float a13 = this.m13;
+		float a20 = this.m20;
+		float a21 = this.m21;
+		float a22 = this.m22;
+		float a23 = this.m23;
+		float a30 = this.m30;
+		float a31 = this.m31;
+		float a32 = this.m32;
+		float a33 = this.m33;
+		float b00 = matrix.m00;
+		float b01 = matrix.m01;
+		float b02 = matrix.m02;
+		float b03 = matrix.m03;
+		float b10 = matrix.m10;
+		float b11 = matrix.m11;
+		float b12 = matrix.m12;
+		float b13 = matrix.m13;
+		float b20 = matrix.m20;
+		float b21 = matrix.m21;
+		float b22 = matrix.m22;
+		float b23 = matrix.m23;
+		float b30 = matrix.m30;
+		float b31 = matrix.m31;
+		float b32 = matrix.m32;
+		float b33 = matrix.m33;
+		this.m00 = a00 * b00 + a01 * b10 + a02 * b20 + a03 * b30;
+		this.m01 = a00 * b01 + a01 * b11 + a02 * b21 + a03 * b31;
+		this.m02 = a00 * b02 + a01 * b12 + a02 * b22 + a03 * b32;
+		this.m03 = a00 * b03 + a01 * b13 + a02 * b23 + a03 * b33;
+		this.m10 = a10 * b00 + a11 * b10 + a12 * b20 + a13 * b30;
+		this.m11 = a10 * b01 + a11 * b11 + a12 * b21 + a13 * b31;
+		this.m12 = a10 * b02 + a11 * b12 + a12 * b22 + a13 * b32;
+		this.m13 = a10 * b03 + a11 * b13 + a12 * b23 + a13 * b33;
+		this.m20 = a20 * b00 + a21 * b10 + a22 * b20 + a23 * b30;
+		this.m21 = a20 * b01 + a21 * b11 + a22 * b21 + a23 * b31;
+		this.m22 = a20 * b02 + a21 * b12 + a22 * b22 + a23 * b32;
+		this.m23 = a20 * b03 + a21 * b13 + a22 * b23 + a23 * b33;
+		this.m30 = a30 * b00 + a31 * b10 + a32 * b20 + a33 * b30;
+		this.m31 = a30 * b01 + a31 * b11 + a32 * b21 + a33 * b31;
+		this.m32 = a30 * b02 + a31 * b12 + a32 * b22 + a33 * b32;
+		this.m33 = a30 * b03 + a31 * b13 + a32 * b23 + a33 * b33;
 	}
 
 	public void multiply(float multiplier) {
@@ -473,7 +489,7 @@ public class Matrix4f {
 		float qz = z * q;
 		float qw = MathHelper.cos(radian * 0.5F);
 
-		// setup matrix
+		// setup rotation matrix
 		float xx = 2.0F * qx * qx;
 		float yy = 2.0F * qy * qy;
 		float zz = 2.0F * qz * qz;
@@ -494,32 +510,32 @@ public class Matrix4f {
 		float r21 = 2.0F * (yz + xw);
 		float r12 = 2.0F * (yz - xw);
 
-		// rotate
-		float f00 = this.m00 * r00 + this.m01 * r10 + this.m02 * r20;
-		float f01 = this.m00 * r01 + this.m01 * r11 + this.m02 * r21;
-		float f02 = this.m00 * r02 + this.m01 * r12 + this.m02 * r22;
-		float f10 = this.m10 * r00 + this.m11 * r10 + this.m12 * r20;
-		float f11 = this.m10 * r01 + this.m11 * r11 + this.m12 * r21;
-		float f12 = this.m10 * r02 + this.m11 * r12 + this.m12 * r22;
-		float f20 = this.m20 * r00 + this.m21 * r10 + this.m22 * r20;
-		float f21 = this.m20 * r01 + this.m21 * r11 + this.m22 * r21;
-		float f22 = this.m20 * r02 + this.m21 * r12 + this.m22 * r22;
-		float f30 = this.m30 * r00 + this.m31 * r10 + this.m32 * r20;
-		float f31 = this.m30 * r01 + this.m31 * r11 + this.m32 * r21;
-		float f32 = this.m30 * r02 + this.m31 * r12 + this.m32 * r22;
+		// multiply matrices
+		float f00 = this.m00;
+		float f01 = this.m01;
+		float f02 = this.m02;
+		float f10 = this.m10;
+		float f11 = this.m11;
+		float f12 = this.m12;
+		float f20 = this.m20;
+		float f21 = this.m21;
+		float f22 = this.m22;
+		float f30 = this.m30;
+		float f31 = this.m31;
+		float f32 = this.m32;
 
-		this.m00 = f00;
-		this.m01 = f01;
-		this.m02 = f02;
-		this.m10 = f10;
-		this.m11 = f11;
-		this.m12 = f12;
-		this.m20 = f20;
-		this.m21 = f21;
-		this.m22 = f22;
-		this.m30 = f30;
-		this.m31 = f31;
-		this.m32 = f32;
+		this.m00 = f00 * r00 + f01 * r10 + f02 * r20;
+		this.m01 = f00 * r01 + f01 * r11 + f02 * r21;
+		this.m02 = f00 * r02 + f01 * r12 + f02 * r22;
+		this.m10 = f10 * r00 + f11 * r10 + f12 * r20;
+		this.m11 = f10 * r01 + f11 * r11 + f12 * r21;
+		this.m12 = f10 * r02 + f11 * r12 + f12 * r22;
+		this.m20 = f20 * r00 + f21 * r10 + f22 * r20;
+		this.m21 = f20 * r01 + f21 * r11 + f22 * r21;
+		this.m22 = f20 * r02 + f21 * r12 + f22 * r22;
+		this.m30 = f30 * r00 + f31 * r10 + f32 * r20;
+		this.m31 = f30 * r01 + f31 * r11 + f32 * r21;
+		this.m32 = f30 * r02 + f31 * r12 + f32 * r22;
 	}
 
 	public void rotateX(float radian, float x) {
@@ -530,7 +546,7 @@ public class Matrix4f {
 		float qx = x * MathHelper.sin(radian * 0.5F);
 		float qw = MathHelper.cos(radian * 0.5F);
 
-		// setup matrix
+		// setup rotation matrix
 		float xx = 2.0F * qx * qx;
 		float xw = qx * qw;
 
@@ -539,24 +555,24 @@ public class Matrix4f {
 		float r21 = 2.0F * xw;
 		float r12 = 2.0F * -xw;
 
-		// rotate
-		float f01 = this.m01 * r11 + this.m02 * r21;
-		float f02 = this.m01 * r12 + this.m02 * r22;
-		float f11 = this.m11 * r11 + this.m12 * r21;
-		float f12 = this.m11 * r12 + this.m12 * r22;
-		float f21 = this.m21 * r11 + this.m22 * r21;
-		float f22 = this.m21 * r12 + this.m22 * r22;
-		float f31 = this.m31 * r11 + this.m32 * r21;
-		float f32 = this.m31 * r12 + this.m32 * r22;
+		// multiply matrices
+		float f01 = this.m01;
+		float f02 = this.m02;
+		float f11 = this.m11;
+		float f12 = this.m12;
+		float f21 = this.m21;
+		float f22 = this.m22;
+		float f31 = this.m31;
+		float f32 = this.m32;
 
-		this.m01 = f01;
-		this.m02 = f02;
-		this.m11 = f11;
-		this.m12 = f12;
-		this.m21 = f21;
-		this.m22 = f22;
-		this.m31 = f31;
-		this.m32 = f32;
+		this.m01 = f01 * r11 + f02 * r21;
+		this.m02 = f01 * r12 + f02 * r22;
+		this.m11 = f11 * r11 + f12 * r21;
+		this.m12 = f11 * r12 + f12 * r22;
+		this.m21 = f21 * r11 + f22 * r21;
+		this.m22 = f21 * r12 + f22 * r22;
+		this.m31 = f31 * r11 + f32 * r21;
+		this.m32 = f31 * r12 + f32 * r22;
 	}
 
 	public void rotateY(float radian, float y) {
@@ -567,7 +583,7 @@ public class Matrix4f {
 		float qy = y * MathHelper.sin(radian * 0.5F);
 		float qw = MathHelper.cos(radian * 0.5F);
 
-		// setup matrix
+		// setup rotation matrix
 		float yy = 2.0F * qy * qy;
 		float yw = qy * qw;
 
@@ -576,24 +592,24 @@ public class Matrix4f {
 		float r20 = 2.0F * -yw;
 		float r02 = 2.0F * yw;
 
-		// rotate
-		float a00 = this.m00 * r00 + this.m02 * r20;
-		float a02 = this.m00 * r02 + this.m02 * r22;
-		float a10 = this.m10 * r00 + this.m12 * r20;
-		float a12 = this.m10 * r02 + this.m12 * r22;
-		float a20 = this.m20 * r00 + this.m22 * r20;
-		float a22 = this.m20 * r02 + this.m22 * r22;
-		float a30 = this.m30 * r00 + this.m32 * r20;
-		float a32 = this.m30 * r02 + this.m32 * r22;
+		// multiply matrices
+		float f00 = this.m00;
+		float f02 = this.m02;
+		float f10 = this.m10;
+		float f12 = this.m12;
+		float f20 = this.m20;
+		float f22 = this.m22;
+		float f30 = this.m30;
+		float f32 = this.m32;
 
-		this.m00 = a00;
-		this.m02 = a02;
-		this.m10 = a10;
-		this.m12 = a12;
-		this.m20 = a20;
-		this.m22 = a22;
-		this.m30 = a30;
-		this.m32 = a32;
+		this.m00 = f00 * r00 + f02 * r20;
+		this.m02 = f00 * r02 + f02 * r22;
+		this.m10 = f10 * r00 + f12 * r20;
+		this.m12 = f10 * r02 + f12 * r22;
+		this.m20 = f20 * r00 + f22 * r20;
+		this.m22 = f20 * r02 + f22 * r22;
+		this.m30 = f30 * r00 + f32 * r20;
+		this.m32 = f30 * r02 + f32 * r22;
 	}
 
 	public void rotateZ(float radian, float z) {
@@ -604,7 +620,7 @@ public class Matrix4f {
 		float qz = z * MathHelper.sin(radian * 0.5F);
 		float qw = MathHelper.cos(radian * 0.5F);
 
-		// setup matrix
+		// setup rotation matrix
 		float zz = 2.0F * qz * qz;
 		float zw = qz * qw;
 
@@ -613,24 +629,24 @@ public class Matrix4f {
 		float r10 = 2.0F * zw;
 		float r01 = 2.0F * -zw;
 
-		// rotate
-		float a00 = this.m00 * r00 + this.m01 * r10;
-		float a01 = this.m00 * r01 + this.m01 * r11;
-		float a10 = this.m10 * r00 + this.m11 * r10;
-		float a11 = this.m10 * r01 + this.m11 * r11;
-		float a20 = this.m20 * r00 + this.m21 * r10;
-		float a21 = this.m20 * r01 + this.m21 * r11;
-		float a30 = this.m30 * r00 + this.m31 * r10;
-		float a31 = this.m30 * r01 + this.m31 * r11;
+		// multiply matrices
+		float f00 = this.m00;
+		float f01 = this.m01;
+		float f10 = this.m10;
+		float f11 = this.m11;
+		float f20 = this.m20;
+		float f21 = this.m21;
+		float f30 = this.m30;
+		float f31 = this.m31;
 
-		this.m00 = a00;
-		this.m01 = a01;
-		this.m10 = a10;
-		this.m11 = a11;
-		this.m20 = a20;
-		this.m21 = a21;
-		this.m30 = a30;
-		this.m31 = a31;
+		this.m00 = f00 * r00 + f01 * r10;
+		this.m01 = f00 * r01 + f01 * r11;
+		this.m10 = f10 * r00 + f11 * r10;
+		this.m11 = f10 * r01 + f11 * r11;
+		this.m20 = f20 * r00 + f21 * r10;
+		this.m21 = f20 * r01 + f21 * r11;
+		this.m30 = f30 * r00 + f31 * r10;
+		this.m31 = f30 * r01 + f31 * r11;
 	}
 
 }
