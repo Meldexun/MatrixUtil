c15484f489e23e90cd047b55003150b773a64cb8
Meldexun <27779321+Meldexun@users.noreply.github.com>
2022-12-09 01:46:44 +0100

    Add method to add matrices

--- src/main/java/meldexun/matrixutil/Matrix4f.java
+++ src/main/java/meldexun/matrixutil/Matrix4f.java
@@ -338,6 +338,25 @@ public class Matrix4f {
 		this.m33 *= multiplier;
 	}
 
+	public void add(Matrix4f matrix) {
+		this.m00 += matrix.m00;
+		this.m01 += matrix.m01;
+		this.m02 += matrix.m02;
+		this.m03 += matrix.m03;
+		this.m10 += matrix.m10;
+		this.m11 += matrix.m11;
+		this.m12 += matrix.m12;
+		this.m13 += matrix.m13;
+		this.m20 += matrix.m20;
+		this.m21 += matrix.m21;
+		this.m22 += matrix.m22;
+		this.m23 += matrix.m23;
+		this.m30 += matrix.m30;
+		this.m31 += matrix.m31;
+		this.m32 += matrix.m32;
+		this.m33 += matrix.m33;
+	}
+
 	public static Matrix4f perspective(double fov, float aspectRatio, float nearPlane, float farPlane) {
 		float f = (float) (1.0D / Math.tan(Math.toRadians(fov) * 0.5D));
 		Matrix4f matrix4f = new Matrix4f();
